59b1f83967bd82f72bb01280d6b695a819338234
sevakul16 <sevakul16@gmail.com>
2024-08-12 13:27:22 +0300

    Added stete for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectState, setPropjectState] = useState({
+    selectedProject: undefined,
+    projects: [],
+  });
+
+  function handleStartNewProject() {
+    setPropjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProject: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProject === null) {
+    content = <NewProject onStartAddProject={handleStartNewProject} />;
+  } else if (projectState.selectedProject === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartNewProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartNewProject} />
+      {content}
     </main>
   );
 }
